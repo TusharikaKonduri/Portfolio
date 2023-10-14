308599e50624039faa5532b4084ad886b219883e
TusharikaKonduri <ksntusharika@gmail.com>
2023-10-14 12:04:49 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Tusharika Konduri",
+    "role": "Frontend Developer",
+    "linkedinId": "TusharikaKonduri",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like exploring in various parts of frontend development and like to learn about new technologies,or simply play games in my free time.This is my portfolio and I developed it as a Task in my MERN course.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/tusharika-konduri-13855b236/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/TusharikaKonduri",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://twitter.com/ksntusharika",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at VIT-AP University and pursuing my B.Tech from here. I am a learning Full Stack Web Developement, and further be diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"VIT-AP University",
+        "specialization":"Artificial Intelligence and Machine Learning - 9.47(currently)",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2025",
+        "Achievements":"Co-lead of Machine Learning Club"
+      },
+      {
+        "UniversityName":"Narayana Junior College",
+        "specialization":"MPC - 97.9%",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+      },
+      {
+        "UniversityName":"VS.ST.JOHN'S Higher Secondary School , Gannavaram",
+        "specialization":"CBSE - 94.2%",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"APOnline",
+        "specialization":"Intern",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2023",
+        "Achievements":"Developing a web application 'EasyRide'"
+      },
+      {
+        "CompanyName":"MERN Full Stack (ongoing)",
+        "specialization":"Certification",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"AWS Cloud Practicioner (ongoing)",
+        "specialization":"Certification",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Project1",
+        "description":"Smart Driverless Bus",
+        "imgurl":"images/car.png"
+      },
+      {
+        "name":"Project2",
+        "description":"IOT project",
+        "imgurl":"images/prototype.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
